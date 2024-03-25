b1151c6b160d4ddaa347752a49fa3a63d4f0b8a0
nokekip <kipnoke@gmail.com>
2024-03-25 19:30:45 +0300

    Find all schools that cover the specified topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/emv python3
+
+"""
+Find all schools that cover the specified topic.
+
+:param mongo_collection: The PyMongo collection object.
+:param topic: The topic to search for.
+:return: A list of schools covering the specified topic.
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ Find all schools that cover the specified topic. """
+    cursor = mongo_collection.find({"topics": topic})
+    return [doc for doc in cursor]
