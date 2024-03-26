63a31c39ce24f1a8a8f23196d43a23cdf75ff88f
nokekip <kipnoke@gmail.com>
2024-03-26 19:57:40 +0300

    update

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,4 +4,14 @@
 
 def top_students(mongo_collection):
     """ Returns all students sorted by average score"""
-    return list(mongo_collection.find())
+    top_student = mongo_collection.aggregate([
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ])
+
+    return top_student
