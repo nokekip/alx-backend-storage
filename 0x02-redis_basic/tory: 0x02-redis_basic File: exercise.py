6c2c00f1ffabba29ecf5763800ee2fafb087de48
nokekip <kipnoke@gmail.com>
2024-03-29 23:12:42 +0300

    Writing strings to Redis

---
+++ 0x02-redis_basic/tory: 0x02-redis_basic File: exercise.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+""" Cache implementation with Redis"""
+
+import redis
+import uuid
+from typing import Union
+
+
+UnionOfTypes = Union[str, int, float, bytes]
+
+
+class Cache:
+    """" class to manage a cache """
+    def __init__(self):
+        """"
+        Initialize the Cache object and flush the Redis database.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: UnionOfTypes) -> str:
+        """
+        Store the input data in Redis using a random key.
+        """
+        self._key = str(uuid.uuid4())
+        self._redis.set(_key, data)
+        return self._key
